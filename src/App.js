22dd97cd7b189397ddb4ac760779790f59c78cbf
nanbratan <morfemi9498@gmail.com>
2019-05-09 17:25:27 +0300

    sas

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          Learn React!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
           Learn React
         </a>
       </header>
